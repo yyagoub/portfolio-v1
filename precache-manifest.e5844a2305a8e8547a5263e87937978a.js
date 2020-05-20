01c8354fcb212bc9f52c46d663f6ab0c441689b5
yyagoub <imjou9@gmail.com>
2020-05-20 18:09:27 +0300

    Updates

---
+++ precache-manifest.e5844a2305a8e8547a5263e87937978a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7a79b2602bf0612c143e3a7cc0c9fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13590f0a77ad50ae1939",
+    "url": "/static/css/main.7076f6e1.chunk.css"
+  },
+  {
+    "revision": "e7ff1adf67c825a7167d",
+    "url": "/static/js/2.ddca2d27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ddca2d27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13590f0a77ad50ae1939",
+    "url": "/static/js/main.8ca45f53.chunk.js"
+  },
+  {
+    "revision": "ee582b84f42facfc0fb6",
+    "url": "/static/js/runtime-main.17f0ce2a.js"
+  }
+]);
